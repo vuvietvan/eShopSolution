c22f9b065c61ebd3e1b094cbd05c8741e9343f12
DESKTOP-0E5V2EQ\ks708 <ks708vant3@gmail.com>
2021-02-25 21:42:15 +0700

    update api & swagger

---
+++ eShopSolution.ViewModels/Catalog/Common/PagingRequestBase.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace eShopSolution.ViewModels.Catalog.Common
+{
+    public class PagingRequestBase
+    {
+        public int PageIndex { get; set; }
+
+        public int PageSize { get; set; }
+    }
+}
