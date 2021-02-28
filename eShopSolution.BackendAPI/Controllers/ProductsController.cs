80728626cddbb5ea3ff7911b6b7fe2bed0f850bb
DESKTOP-0E5V2EQ\ks708 <ks708vant3@gmail.com>
2021-02-28 16:57:54 +0700

    update Fluent Validation

--- eShopSolution.BackendAPI/Controllers/ProductsController.cs
+++ eShopSolution.BackendAPI/Controllers/ProductsController.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 using eShopSolution.Application.Catalog.Products;
 using eShopSolution.ViewModels.Catalog.ProductImages;
 using eShopSolution.ViewModels.Catalog.Products;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
@@ -12,6 +13,7 @@ namespace eShopSolution.BackendAPI.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
+    [Authorize]
     public class ProductsController : ControllerBase
     {
         private readonly IPublicProductService _publicProductService;
