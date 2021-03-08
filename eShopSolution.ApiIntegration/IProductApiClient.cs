63dad0851d8455c2a3cbbce4d0f4bee40045777c
DESKTOP-0E5V2EQ\ks708 <ks708vant3@gmail.com>
2021-03-08 13:51:04 +0700

    update_product

--- eShopSolution.ApiIntegration/IProductApiClient.cs
+++ eShopSolution.ApiIntegration/IProductApiClient.cs
@@ -13,6 +13,8 @@ namespace eShopSolution.ApiIntegration
 
         Task<bool> CreateProduct(ProductCreateRequest request);
 
+        Task<bool> UpdateProduct(ProductUpdateRequest request);
+
         Task<ApiResult<bool>> CategoryAssign(int id, CategoryAssignRequest request);
 
         Task<ProductVm> GetById(int id, string languageId);
