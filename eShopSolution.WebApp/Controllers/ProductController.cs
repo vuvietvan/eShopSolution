71d91da1aa5e2dade2ba212a040fca27a452cc64
DESKTOP-0E5V2EQ\ks708 <ks708vant3@gmail.com>
2021-03-08 13:29:13 +0700

    category_product

---
+++ eShopSolution.WebApp/Controllers/ProductController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace eShopSolution.WebApp.Controllers
+{
+    public class ProductController : Controller
+    {
+        public IActionResult Detail(int id)
+        {
+            return View();
+        }
+
+        public IActionResult Category(int id)
+        {
+            return View();
+        }
+    }
+}
